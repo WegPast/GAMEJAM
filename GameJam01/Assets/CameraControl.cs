36b3c766663223a8043a90beac5bd98ad2f2a657
WegPast <wegpast@gmail.com>
2018-03-18 00:04:22 +0100

    Arena revamp CameraControl

---
+++ GameJam01/Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        if (!player) {
+            Debug.LogError("No player attached to main camera !");
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (player) {
+            Vector3 playerPos = player.gameObject.transform.position;
+            transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+        } else {
+
+            transform.position = new Vector3(0f, 0f, transform.position.z);
+        }
+    }
+}
