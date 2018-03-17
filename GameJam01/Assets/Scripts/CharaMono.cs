4d7c3d5153d27206fe1778e3cad919e52a986c5d
WegPast <wegpast@gmail.com>
2018-03-17 17:05:37 +0100

    CharaMono modifcation

--- GameJam01/Assets/Scripts/CharaMono.cs
+++ GameJam01/Assets/Scripts/CharaMono.cs
@@ -6,38 +6,31 @@ using UnityEngine.Networking;
 public class CharaMono : MonoBehaviour
 {
 
-    private float speed = 1.0F;
+    public float speed = 1.0F;
 
     //Synchronized variables
 
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
 
     }
 
-    void Update()
-    {
-		move ();
+    void Update() {
+        move();
     }
 
-    void move()
-    {
-        if (Input.GetKey(KeyCode.Z))
-        {
+    void move() {
+        if (Input.GetKey(KeyCode.Z)) {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.S)) {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)) {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.Q))
-        {
+        if (Input.GetKey(KeyCode.Q)) {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
     }
