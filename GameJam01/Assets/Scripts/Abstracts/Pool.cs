f458b3086b07cdc05de881e5428f1ec79773e174
landry_berry <landryberry@gmail.com>
2018-03-17 22:28:30 +0100

    Gestion de pool

---
+++ GameJam01/Assets/Scripts/Abstracts/Pool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pool<T> : MonoBehaviour where T : GameObject {
+
+	protected List<T> pool;
+
+	protected abstract void Execute ();
+	
+	// Update is called once per frame
+	void Update () {
+		Execute ();
+	}
+
+	public void add(T newPlayer) {
+		if (pool == null) {
+			pool = new List<T>();
+		}
+		pool.Add (newPlayer);
+	}
+
+	public class DefaultImp : Pool<GameObject> {
+	}
+}
