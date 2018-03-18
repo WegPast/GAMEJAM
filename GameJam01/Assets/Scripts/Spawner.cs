80e0016646aaefe18f12975cc7159003aa8f6dc4
landry_berry <landryberry@gmail.com>
2018-03-18 04:26:33 +0100

    init wave manager

--- GameJam01/Assets/Scripts/Spawner.cs
+++ GameJam01/Assets/Scripts/Spawner.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
-    public GameObject entityToSpawn;
-	public float spawnPerSecond = 5f;
-	private float nextUpdate;
-
     // Use this for initialization
     void Start() {
-		if (entityToSpawn == null) {
-			Debug.LogError ("No entities to spawn for " + name);
-		}
     }
 
     // Update is called once per frame
     void Update() {
-		if (Time.time >= nextUpdate) {
-			nextUpdate = Mathf.FloorToInt (Time.time) + spawnPerSecond;
-			Spawn (entityToSpawn);
-		}
+
     }
 
 	void Spawn(GameObject toSpawn) {
