c264d749e64f4a42b7d46d0b0e74cc7f53635103
WegPast <wegpast@gmail.com>
2018-04-12 13:53:32 +0200

    Creating weapon script

---
+++ GameJam01/Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Weapon : NetworkBehaviour {
+
+    public Projectiles projectileType; // Contains damage, speed etc...
+    public float fireRate;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FireProjectile(Quaternion gunRotation) {
+        GameObject bullet1 = Instantiate(projectileType.gameObject, transform.position, Quaternion.identity) as GameObject;
+        bullet1.GetComponent<Projectiles>().Fire(gunRotation);
+        
+        if (isLocalPlayer) {
+            bullet1.GetComponent<Projectiles>().isFromMyPlayer = true;
+        }
+    }
+}
