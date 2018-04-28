349cc0ea4020b6054a03d3ddaa313cad0692c947
landry_berry <landryberry@gmail.com>
2018-04-28 12:30:25 +0200

    modification de l'ennemy

--- GameJam01/Assets/Scripts/Ennemy.cs
+++ GameJam01/Assets/Scripts/Ennemy.cs
@@ -5,67 +5,70 @@ using UnityEngine.Networking;
 
 public class Ennemy : NetworkBehaviour
 {
+    [
+        Header("Drop chance of bonus"),
+        RangeAttribute(0, 100)
+    ]
+    public int dropChance;
+    
+    [Header("Enemy characteristic")]
+    public float movementSpeed = 1F;
 
-    //Synchronized variables
-    [SyncVar] Vector2 synchronizedPosition;
-    [SyncVar] Quaternion synchronizedRotation;
+    [Header("Prefab")]
+    public GameObject prefab;
 
     private PlayerControl currentTarget;
     private GameManager gameManager;
+    private int lifeManager;
     private float contactDist = 0F;
-
-    [Header("Enemy characteristic")]
-    public float movementSpeed = 1F;
 
     // Use this for initialization
     void Start() {
         //movementSpeed = 0.05F;
         gameManager = GameObject.FindObjectOfType<GameManager>();
+        lifeManager = this.GetComponent<int>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (!isServer)
+        if (true) {
+            Debug.Log("Aaaargh je meuuuuuuuuuuuuuuuuur!");
+            // Si je meur je laisse potentiellement une caisse à l'endroit de ma mort.
+            // Ici on créé notre caisse seulement si random%100 > chance drop
+            if (dropChance == 100 || Random.Range(0, 101) <= dropChance) {
+                Debug.Log("youhou on va spawn une caisse");
+                Instantiate(prefab, new Vector3(), Quaternion.identity);
+            }
+        }
+
+        if (currentTarget != null)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -5f);
-            transform.position = Vector3.Lerp(transform.position, synchronizedPosition, Time.deltaTime * 10);
-            transform.Find("enemy_sprite").rotation = synchronizedRotation;
+            if (Vector2.Distance(transform.position, currentTarget.transform.position) >= contactDist)
+            {
+                // Init mouvement guide line
+                Vector2 axe = currentTarget.transform.position - gameObject.transform.position;
+                axe.Normalize();
+                gameObject.transform.Translate(axe * movementSpeed * Time.deltaTime);
+
+                Vector3 difference = currentTarget.transform.position - transform.position;
+                difference.Normalize();
+                float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+                gameObject.transform.Find("enemy_sprite").transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+            }
         }
         else
         {
-            if (currentTarget != null)
+            PlayerControl[] potentialTarget = GameObject.FindObjectsOfType<PlayerControl>();
+            if (potentialTarget != null && potentialTarget.Length > 0)
             {
-                if (Vector2.Distance(transform.position, currentTarget.transform.position) >= contactDist)
-                {
-                    // Init mouvement guide line
-                    Vector2 axe = currentTarget.transform.position - gameObject.transform.position;
-                    axe.Normalize();
-                    gameObject.transform.Translate(axe * movementSpeed * Time.deltaTime);
-
-                    Vector3 difference = currentTarget.transform.position - transform.position;
-                    difference.Normalize();
-                    float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-                    gameObject.transform.Find("enemy_sprite").transform.rotation = Quaternion.Euler(0f, 0f, rotation);
-                }
+                int luckyBastard = Random.Range(1, potentialTarget.Length + 1);
+                currentTarget = potentialTarget[luckyBastard - 1];
             }
-            else
-            {
-                PlayerControl[] potentialTarget = GameObject.FindObjectsOfType<PlayerControl>();
-                if (potentialTarget != null && potentialTarget.Length > 0)
-                {
-                    int luckyBastard = Random.Range(1, potentialTarget.Length + 1);
-                    currentTarget = potentialTarget[luckyBastard - 1];
-                }
-            }
-            //Network synchronisation
-            SendPosition();
         }
-        
     }
 
-
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.GetComponent<Projectiles>()){
+        if (collision.gameObject.GetComponent<Projectiles>()) {
             if (collision.gameObject.GetComponent<Projectiles>().isFromMyPlayer) {
                 GameManager.nbEnnemiesKilled++;
             }
@@ -73,31 +76,6 @@ public class Ennemy : NetworkBehaviour
         }
         if (collision.gameObject.GetComponent<PlayerControl>()) {
             Destroy(gameObject);
-
         }
-    }
-
-        //Network function
-
-    //Client
-    [Client]
-    void SendPosition()
-    {
-        CmdSendMyPositionToServer(transform.position);
-        CmdSendMyRotationToServer(transform.Find("enemy_sprite").rotation);
-    }
-
-
-    //Command
-    [Command]
-    void CmdSendMyPositionToServer(Vector3 position)
-    {
-        synchronizedPosition = position;
-    }
-
-    [Command]
-    void CmdSendMyRotationToServer(Quaternion rotation)
-    {
-        synchronizedRotation = rotation;
     }
 }
