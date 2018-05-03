9962ee9e8101974dec53559007098f0fec28ad61
WegPast <wegpast@gmail.com>
2018-05-03 13:37:16 +0200

    WIP shop (do not care about "manifest.json" its only a metada for swtching from unity 2017 to 2018)

--- GameJam01/Assets/Scripts/ShopManager.cs
+++ GameJam01/Assets/Scripts/ShopManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopManager : MonoBehaviour {
+public class ShopManager : MonoBehaviour
+{
 
   [Header("Left side")]
   public Image uiWeaponSpriteLeft;
@@ -14,19 +15,68 @@ public class ShopManager : MonoBehaviour {
   [Header("Available weapons")]
   public Weapon[] availableWeapons;
 
-  // Use this for initialization
-  void Start () {
-    foreach (var item in availableWeapons) {
+  private int currentSelectedLeftWeaponIndex = 0;
+  private int currentSelectedRightWeaponIndex = 0;
 
-      Debug.Log("--- Weapon : "+item.name+"----");
-      Debug.Log("Description : \n"+item.description);
-      Debug.Log("Firerate : "+item.fireRate);
-      Debug.Log("Projectile : "+item.projectileType.name);
+  // Use this for initialization
+  void Start() {
+    //foreach (var item in availableWeapons) {
+
+    //  Debug.Log("--- Weapon : " + item.name + " ---");
+    //  Debug.Log("Description : \n" + item.description);
+    //  Debug.Log("Firerate : " + item.fireRate);
+    //  Debug.Log("Projectile : " + item.projectileType.name);
+    //}
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void NextWeapon(string side) {
+    if (side == "left") {
+      if ((currentSelectedLeftWeaponIndex + 1) >= availableWeapons.Length) {
+        currentSelectedLeftWeaponIndex = 0;
+      } else {
+        currentSelectedLeftWeaponIndex++;
+      }
+      GameObject weaponBody = availableWeapons[currentSelectedLeftWeaponIndex].transform.Find("Body").gameObject;
+      uiWeaponSpriteLeft.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+      hovercraftWeaponSpriteLeft.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+    } else {
+      if ((currentSelectedRightWeaponIndex + 1) >= availableWeapons.Length) {
+        currentSelectedRightWeaponIndex = 0;
+      } else {
+        currentSelectedRightWeaponIndex++;
+      }
+      GameObject weaponBody = availableWeapons[currentSelectedRightWeaponIndex].transform.Find("Body").gameObject;
+      uiWeaponSpriteRight.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+      hovercraftWeaponSpriteRight.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
     }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  }
+
+  public void PreviousWeapon(string side) {
+    if (side == "left") {
+      if ((currentSelectedLeftWeaponIndex - 1) < 0) {
+        currentSelectedLeftWeaponIndex = availableWeapons.Length - 1;
+      } else {
+        currentSelectedLeftWeaponIndex--;
+      }
+      GameObject weaponBody = availableWeapons[currentSelectedLeftWeaponIndex].transform.Find("Body").gameObject;
+      uiWeaponSpriteLeft.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+      hovercraftWeaponSpriteLeft.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+
+    } else {
+
+      if ((currentSelectedRightWeaponIndex - 1) >= availableWeapons.Length) {
+        currentSelectedRightWeaponIndex = 0;
+      } else {
+        currentSelectedRightWeaponIndex++;
+      }
+      GameObject weaponBody = availableWeapons[currentSelectedRightWeaponIndex].transform.Find("Body").gameObject;
+      uiWeaponSpriteRight.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+      hovercraftWeaponSpriteRight.sprite = weaponBody.GetComponent<SpriteRenderer>().sprite;
+    }
+  }
 }
