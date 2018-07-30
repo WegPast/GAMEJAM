37d77dd1b1ff857e14b654e23674eaf77cc5b4c1
WegPast <wegpast@gmail.com>
2018-07-30 14:06:21 +0200

    Try to cast 2D shadow...

---
+++ GameJam01/Assets/ShadowRenderer2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowRenderer2D : MonoBehaviour {
+
+  public float yOffset = 10f;
+  public Sprite shadowSprite;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
