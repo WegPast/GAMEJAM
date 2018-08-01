15f78b2fd643dc6ca90947cfba6a300e6cf09ab4
WegPast <wegpast@gmail.com>
2018-08-01 14:08:40 +0200

    SHADOW OK !

---
+++ GameJam01/Assets/Scripts/ShadowRenderer2D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowRenderer2D : MonoBehaviour {
+
+  public Vector2 offest = new Vector2(0f, -0.2f);
+  public Material shadowMaterial;
+  public Color shadowColor;
+
+  private SpriteRenderer spriteRndCaster;
+  private SpriteRenderer spriteRndShadow;
+
+  private Transform transCaster;
+  private Transform transShadow;
+
+  private void Start() {
+    transCaster = transform;
+    transShadow = new GameObject("shadow").transform;
+    transShadow.parent = transCaster;
+    transShadow.localRotation = Quaternion.identity;
+
+    spriteRndCaster = GetComponent<SpriteRenderer>();
+    spriteRndShadow = transShadow.gameObject.AddComponent<SpriteRenderer>();
+
+    spriteRndShadow.sortingLayerName = spriteRndCaster.sortingLayerName;
+    spriteRndShadow.sortingOrder = spriteRndCaster.sortingOrder - 1;
+
+    spriteRndShadow.material = shadowMaterial;
+    spriteRndShadow.color = shadowColor;
+
+  }
+
+  private void LateUpdate() {
+    transShadow.position = new Vector2(transCaster.position.x + offest.x, transCaster.position.y + offest.y);
+    spriteRndShadow.sprite = spriteRndCaster.sprite;
+  }
+
+}
